194ed959c2e464d03473cbba881895fd612b1aa1
kevineriksson <kevin.eriksson@sunly.ee>
2025-07-04 13:38:57 +0300

    fixed file path

--- src/config.py
+++ src/config.py
@@ -2,17 +2,17 @@ from pathlib import Path
 
 DATA_DIR = Path(__file__).resolve().parent / "data"
 
-PROFILES_EE = DATA_DIR / "profiles_EE.xlsx"
-PROFILES_LV = DATA_DIR / "profiles_LV.xlsx"
-PROFILES_LT = DATA_DIR / "profiles_LT.xlsx"
-PROFILES_PL = DATA_DIR / "profiles_PL.xlsx"
+PROFILES_EE = Path("src/data/profiles_EE.xlsx")
+PROFILES_LV = Path("src/data/profiles_LV.xlsx")
+PROFILES_LT = Path("src/data/profiles_LT.xlsx")
+PROFILES_PL = Path("src/data/profiles_PL.xlsx")
 
-YIELD_EE = DATA_DIR / "YIELD_EE.xlsx"
-YIELD_LV = DATA_DIR / "YIELD_LV.xlsx"
-YIELD_LT = DATA_DIR / "YIELD_LT.xlsx"
-YIELD_PL = DATA_DIR / "YIELD_PL.xlsx"
+YIELD_EE = Path("src/data/YIELD_EE.xlsx")
+YIELD_LV = Path("src/data/YIELD_LV.xlsx")
+YIELD_LT = Path("src/data/YIELD_LT.xlsx")
+YIELD_PL = Path("src/data/YIELD_PL.xlsx")
 
-PV_WIND_PROD_EE = DATA_DIR / "PV_WIND_PROD_EE.xlsx"
-PV_WIND_PROD_LV = DATA_DIR / "PV_WIND_PROD_LV.xlsx"
-PV_WIND_PROD_LT = DATA_DIR / "PV_WIND_PROD_LT.xlsx"
-PV_WIND_PROD_PL = DATA_DIR / "PV_WIND_PROD_PL.xlsx"
+PV_WIND_PROD_EE = Path("src/data/PV_WIND_PROD_EE.xlsx")
+PV_WIND_PROD_LV = Path("src/data/PV_WIND_PROD_LV.xlsx")
+PV_WIND_PROD_LT = Path("src/data/PV_WIND_PROD_LT.xlsx")
+PV_WIND_PROD_PL = Path("src/data/PV_WIND_PROD_PL.xlsx")
