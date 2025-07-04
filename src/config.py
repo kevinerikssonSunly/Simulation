fddd22b8106a2bcdb2b75ac210bb36990619257a
kevineriksson <kevin.eriksson@sunly.ee>
2025-07-04 13:31:12 +0300

    added data to source folder

--- src/config.py
+++ src/config.py
@@ -1,17 +1,17 @@
 from pathlib import Path
 
-PROFILES_EE = Path("/Users/kevineriksson/PycharmProjects/Simulation/data/profiles_EE.xlsx")
-PROFILES_LV = Path("/Users/kevineriksson/PycharmProjects/Simulation/data/profiles_LT.xsx")
-PROFILES_LT = Path("/Users/kevineriksson/PycharmProjects/Simulation/data/profiles_LV.xlsx")
-PROFILES_PL = Path("/Users/kevineriksson/PycharmProjects/Simulation/data/profiles_PL.xlsx")
+PROFILES_EE = Path("/data/profiles_EE.xlsx")
+PROFILES_LV = Path("/data/profiles_LT.xsx")
+PROFILES_LT = Path("/data/profiles_LV.xlsx")
+PROFILES_PL = Path("/data/profiles_PL.xlsx")
 
-YIELD_EE = Path("/Users/kevineriksson/PycharmProjects/Simulation/data/YIELD_EE.xlsx")
-YIELD_LV = Path("/Users/kevineriksson/PycharmProjects/Simulation/data/YIELD_LV.xlsx")
-YIELD_LT = Path("/Users/kevineriksson/PycharmProjects/Simulation/data/YIELD_LT.xlsx")
-YIELD_PL = Path("/Users/kevineriksson/PycharmProjects/Simulation/data/YIELD_PL.xlsx")
+YIELD_EE = Path("/data/YIELD_EE.xlsx")
+YIELD_LV = Path("/data/YIELD_LV.xlsx")
+YIELD_LT = Path("/data/YIELD_LT.xlsx")
+YIELD_PL = Path("/data/YIELD_PL.xlsx")
 
-PV_WIND_PROD_EE = Path("/Users/kevineriksson/PycharmProjects/Simulation/data/PV_WIND_PROD_EE.xlsx")
-PV_WIND_PROD_LV = Path("/Users/kevineriksson/PycharmProjects/Simulation/data/PV_WIND_PROD_LV.xlsx")
-PV_WIND_PROD_LT = Path("/Users/kevineriksson/PycharmProjects/Simulation/data/PV_WIND_PROD_LT.xlsx")
-PV_WIND_PROD_PL = Path("/Users/kevineriksson/PycharmProjects/Simulation/data/PV_WIND_PROD_PL.xlsx")
+PV_WIND_PROD_EE = Path("/data/PV_WIND_PROD_EE.xlsx")
+PV_WIND_PROD_LV = Path("/data/PV_WIND_PROD_LV.xlsx")
+PV_WIND_PROD_LT = Path("/data/PV_WIND_PROD_LT.xlsx")
+PV_WIND_PROD_PL = Path("/data/PV_WIND_PROD_PL.xlsx")
 
